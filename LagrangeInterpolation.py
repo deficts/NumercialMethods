605f288af9b81a938bcf73d075cfdf22538e8051
deficts <diegoar7@live.com>
2019-11-15 19:10:06 -0600

    Interpolacion de Lagrange en python

---
+++ LagrangeInterpolation.py
@@ -0,0 +1,28 @@
+from sympy import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Recibimos dos arreglos con los valores de x & y
+def crearPolinomio(x,y):
+    n=len(x)
+    polinomio=''
+    #Recorremos los arreglos para generar la expresion en string de la sumatoria de la forma
+    #(x-xj)/(xi-xj)
+    for i in range(n):
+        polinomio+= str(y[i])+'* ('
+        denominador=1
+        for j in range(n):
+            if(j==i):
+                continue
+            polinomio+='(x - %f)*'%(x[j])
+            denominador*=x[i]-x[j]
+        polinomio=polinomio[:-1] + '/%f) +'%(denominador)
+    polinomio=polinomio[:-1]
+    #Pasamos la expresion a sympy para ser simplificada y expandida
+    polinomio=sympify(polinomio)
+    polinomio=expand(polinomio)
+    #Regresamos el polinomio creado
+
+    return poly(polinomio).coeffs()
+
+print(crearPolinomio([2,3,4,5,6,7],[0.5,.333,.25,.2,.1667,.1429]))
