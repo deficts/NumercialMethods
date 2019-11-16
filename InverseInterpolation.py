4f17b84fc77ca8d7f92685ef9d66e41b96c0e462
deficts <diegoar7@live.com>
2019-11-15 19:47:22 -0600

    Terminando interpolacion inversa

--- InverseInterpolation.py
+++ InverseInterpolation.py
@@ -1,5 +1,17 @@
 import Bairstow as bstw
 import LagrangeInterpolation as lagInt
+import random
 
-def inverseInterpolation(x,y):
+def inverseInterpolation(x,y,fx):
     polinomial=lagInt.crearPolinomio(x,y)
+    polinomial.reverse()
+    polinomial[0]=polinomial[0]-fx
+    roots=[]
+    r=random.random()
+    s=random.random()
+    bstw.bairstow(polinomial,r,s,roots)
+    return roots
+x=[1,2,3]
+y=[3,2,1]
+
+print(inverseInterpolation(x,y,4))
