fc26c3e4c4d48a66947bdf5a642e2f78fa022538
deficts <diegoar7@live.com>
2019-12-01 17:40:53 -0600

    Creando algoritmo de simpson para calcular integrales dado un arreglo de puntos

---
+++ Simpson.py
@@ -0,0 +1,35 @@
+def simpson38(h,f0,f1,f2,f3):
+    return (3/8)*h*(f0+3*f1+3*f2+f3)
+
+def simpson13(h,n,f):
+    sum=f[0]
+    for i in range(1,n-2,2):
+        sum=sum+4*f[i]+2*f[i+1]
+    sum=sum+4*f[n-1]+f[n]
+    return(h/3)*sum
+
+def intsim(a,b,n,f):
+    h=(b-a)/n
+    sum=0
+    if n==1:
+        sum=(h/2)*(f[n-1]+f[n])
+    else:
+        m=n
+        odd=n%2
+        if odd>0 and n>1:
+            sum=sum + simpson38(h,f[n-3],f[n-2],f[n-1],f[n])
+            m=n-3
+        if m>1:
+            #print("and this is crazy")
+            sum=sum+simpson13(h,m,f)
+            #print(sum)
+    return sum
+
+def simpson(a):
+    if(len(a)<1 and len(a[0][1])<1):
+        return -1
+    return intsim(a[0][0],a[0][len(a[0])-1],len(a[0])-1,a[1])
+
+a=[[-3,-1,1,3,5],[-3375,-343,1,729,4913]]
+
+print(simpson(a))
